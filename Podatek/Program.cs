64ef49e205ee4ec2a4c1fec0cedf5e3d558ece78
Alena Mikhalkevich <emixalkevich4@gmail.com>
2022-05-26 23:10:01 +0200

    Add project files.

---
+++ Podatek/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Podatek
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const decimal VAT = 0.22M;
+
+            decimal netto;
+
+            Console.Write("Podaj cenę netto: ");
+            netto = Convert.ToDecimal(Console.ReadLine());
+
+            decimal podatek = VAT * netto;
+            decimal brutto = netto + podatek;
+
+            Console.WriteLine("Cena wynosi {0} zł, w tym kwota podatku {1} zł.", brutto, podatek);
+            Console.ReadKey();
+        }
+    }
+}
